893b15452d3139dc202def55c6e680ee7d93eea3
Castagnna <daniel.c4stagna@gmail.com>
2023-07-23 12:55:10 -0300

    using secrets

--- iron/streamlit_app.py
+++ iron/streamlit_app.py
@@ -2,38 +2,39 @@ import streamlit as st
 import pandas as pd
 from pymongo import MongoClient
 
-user = "foreigner"
-password = "foreigner"
-MONGODB_URI = f"mongodb+srv://{user}:{password}@pump.u3anmtq.mongodb.net/?retryWrites=true&w=majority"
+user = st.secrets.mongo.user
+password = st.secrets.mongo.password
 
-mg = MongoClient(MONGODB_URI)
+@st.cache_resource
+def init_connection():
+    return MongoClient(f"mongodb+srv://{user}:{password}@pump.u3anmtq.mongodb.net/?retryWrites=true&w=majority")
 
-anthropometry = mg.pump.anthropometry
+client = init_connection()
 
-where = {"user_id": "107721031806"}
+@st.cache_data(ttl=600)
+def get_data():
+    where = {"user_id": "107721031806"}
+    select = {
+        "dataOrdem": 1,
+        "massa": 1,
+        "gc": 1,
+        "massaLivre": 1,
+        "peso_g": 1,
+        "peso_r": 1,
+        "estatura": 1,
+        "somatorio": 1,
+        'c_torax': 1,
+        'c_ombro': 1,
+        'c_cintura': 1,
+        'c_abdomen': 1,
+        'c_quadril': 1,
+    }
+    anthropometry = client.pump.anthropometry
+    items = anthropometry.find(where, select)
+    return pd.DataFrame(data=list(items)).drop(columns={"_id"})
 
-select = {
-    "dataOrdem": 1,
-    "massa": 1,
-    "gc": 1,
-    "massaLivre": 1,
-    "peso_g": 1,
-    "peso_r": 1,
-    "estatura": 1,
-    "somatorio": 1,
-    'c_torax': 1,
-    'c_ombro': 1,
-    'c_cintura': 1,
-    'c_abdomen': 1,
-    'c_quadril': 1,
-}
+df = get_data()
 
-cursor = anthropometry.find(where, select)
-
-measurements = list(cursor)
-
-df = pd.DataFrame(data=measurements).drop(columns={"_id"})
-
-st.write("stringggg")
+st.write("stringGgg")
 st.write(df)
 st.table(df)
