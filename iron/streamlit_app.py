2f9ccbb496229075f67e1d4c184f5cd405f7c653
Daniel CS <56129919+Castagnna@users.noreply.github.com>
2024-05-25 12:48:55 -0300

    Dev (#11)
    * charts
    
    * title
    
    * renaming columns
    
    * exposing Streamlit's foreign secrets
    
    * updating requirements
    
    * streamlit development stream
    
    * updating readme
    
    * organizing the project
    
    * about
    
    * two axes line chart
    
    * Update graphs

--- iron/streamlit_app.py
+++ iron/streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from conn import mongo_client as client
 from data import get_data
-from figures import plot_two_axes_line_chart
+from figures import plot_two_axes_line_chart, plot_stacked_bar_and_line_chart
 
 
 data = get_data(client, "107721031806")
@@ -11,17 +11,17 @@ st.title("Anthropometry dashboard")
 start, end = st.select_slider(
     "Select a range of dates",
     options=data.index,
-    value=(min(data.index), max(data.index)),
+    value=(sorted(data.index)[2], max(data.index)),
 )
 
 y1 = st.selectbox("Select Y1", data.columns, index=0)
 
-y2 = st.selectbox("Select Y2", data.columns, index=len(data.columns)-3)
+y2 = st.selectbox("Select Y2", data.columns, index=len(data.columns)-5)
 
 filtered = data.loc[(data.index >= start) & (data.index <= end)]
 
-tab1, tab2 = st.tabs(["Comparison", "Trend"])
+tab1, tab2 = st.tabs(["Comparison", "Mass and body fat"])
 with tab1:
     plot_two_axes_line_chart(filtered, y1, y2)
 with tab2:
-    plot_two_axes_line_chart(filtered, y1, y2)
+    plot_stacked_bar_and_line_chart(filtered)
